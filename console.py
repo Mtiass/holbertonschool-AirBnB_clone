6fc6aab65d3b3ec834c976535a78b042a922f3b4
Mtiass <mdlsproff@gmail.com>
2024-02-26 20:09:48 +0000

    console

---
+++ console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a program that contains the entry point of
+the command interpreter.
+"""
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
