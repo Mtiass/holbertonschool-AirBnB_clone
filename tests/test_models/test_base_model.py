be77c2f31746ed16390f3027f52087a96b7d783b
Alberto Riffaud <alriffaud@gmail.com>
2024-02-26 21:05:58 -0300

    Modification of tests/test_models/test_base_model.py file.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,6 +4,7 @@ Unittest for BaseModel class.
 """
 import unittest
 from models.base_model import BaseModel
+from time import sleep
 
 
 class BaseModel_Test(unittest.TestCase):
@@ -14,6 +15,15 @@ class BaseModel_Test(unittest.TestCase):
         """Test for correct class type"""
         b = BaseModel()
         self.assertEqual(b.__class__.__name__, "BaseModel")
+
+    def test_save(self):
+        """Test to validate that updated_at is changed when saved"""
+        b = BaseModel()
+        first_time = b.updated_at
+        sleep(.5)
+        b.save()
+        second_time = b.updated_at
+        self.assertNotEqual(first_time, second_time)
 
 
 if __name__ == '__main__':
