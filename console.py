34be533fd426287decb08d1f51f10b1544af62b2
Mtiass <MDLSproff@gmail.com>
2024-02-28 14:43:50 -0300

    console.py

--- console.py
+++ console.py
@@ -4,9 +4,39 @@ This module defines a program that contains the entry point of
 the command interpreter.
 """
 import cmd
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
+    """
+    This is a class that works as command interpreter.
+    """
+    prompt = "(hbnb)"
 
+    def _quit(self, arg):
+        """
+        This method exits the program.
+        """
+        return (True)
+
+    def _EOF(self, arg):
+        """
+        This method exits the program.
+        """
+        return (True)
+
+    def emptyline(self):
+        """
+        This method is for when an empty line + Enter is pressed
+        to do nothing.
+        """
+        pass
+
+    def _create(self, arg):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id.
+        """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
